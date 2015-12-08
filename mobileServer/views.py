5bfd068b8ef3a33a5f86be8fd3e49b14f9815236
ahmed-emam <aemam.douh@mail.com>
2015-12-09 00:09:54 +0300

    Decorator for Shop View

--- mobileServer/views.py
+++ mobileServer/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from mobileServer.models import *
 from mobileServer.serializer import ShopSerializer
-
+from rest_framework.decorators import api_view
 # Create your views here.
 def index(request):
     page = render(request, "index.html", {})
@@ -12,6 +12,7 @@ def getshops(request):
     return
 
 
+@api_view(['GET'])
 # ViewSets define the view behavior.
 class ShopViewSet(viewsets.ModelViewSet):
     queryset = Shop.objects.all()
