27deed666b4ec605c64ad2a2363630b93ff6e5ee
ahmed-emam <aemam.douh@mail.com>
2016-01-23 14:31:29 +0300

    Adding database propagation

---
+++ propagateDatabase/propagateProducts.py
@@ -0,0 +1,19 @@
+import requests
+url = 'http://104.236.115.239:8001/auth/register/'
+
+
+def main():
+    print("Propagating products")
+    with open("list_products") as file:
+        listOfProducts = file.readlines()
+        # print (listOfProducts)
+        for row in listOfProducts:
+            row = row.strip().split()
+            # print(row)
+            payload = {'name': row[0], 'company': row[1], 'category': row[2], 'img': row[3]}
+            request = requests.post(url, data=payload)
+            print("Requested: "+request.url)
+            print(request.status_code)
+            print(request.json())
+
+main()
