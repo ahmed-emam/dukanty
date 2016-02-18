e225d4ef487d9ae9dc9515f8dd8163ec5aa54f3f
ahmed-emam <aemam.douh@mail.com>
2016-02-19 00:29:18 +0300

    Update

--- mobileServer/inventoryUtils.py
+++ mobileServer/inventoryUtils.py
@@ -26,7 +26,7 @@ class JSONResponse(HttpResponse):
 @authentication_classes((TokenAuthentication,))
 @permission_classes((IsAuthenticated,))
 def get_shopInventory(request):
-    shop_id = request.GET.get['shop_id']
+    shop_id = request.GET.get('shop_id')
     # Get the shop using shop ID
     try:
         shop = MobileserverShop.objects.get(pk=shop_id)
