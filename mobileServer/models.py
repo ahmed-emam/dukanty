a31945e8a7ef541bc86b57d038f9fa8a0125cead
ahmed-emam <aemam.douh@mail.com>
2016-02-25 22:40:07 +0300

    Update

--- mobileServer/models.py
+++ mobileServer/models.py
@@ -2,40 +2,40 @@ from django.db import models
 from taggit.managers import TaggableManager
 from users.models import UsersCustomUser
 from django.utils.translation import ugettext_lazy as _
-
+ 
 # User = get_user_model()
 #
-
-
-
-
+ 
+ 
+ 
+ 
 class MobileserverBasket(models.Model):
     name = models.CharField(max_length=30)
     owner = models.ForeignKey('users.UsersCustomuser', models.DO_NOTHING)
     cart = models.ForeignKey('MobileserverCart', models.DO_NOTHING, blank=True, null=True)
-
+ 
     class Meta:
         db_table = 'mobileServer_basket'
-
-
+ 
+ 
 class MobileserverCart(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     checked_out = models.BooleanField()
     owner = models.ForeignKey('users.UsersCustomuser')
-
+ 
     class Meta:
         db_table = 'mobileServer_cart'
-
-
+ 
+ 
 class MobileserverCartitem(models.Model):
     quantity = models.PositiveSmallIntegerField()
-    cart = models.ForeignKey(MobileserverCart)
+    cart = models.ForeignKey(MobileserverCart, on_delete=models.CASCADE)
     product = models.ForeignKey('MobileserverShopproductinventory', models.DO_NOTHING)
-
+ 
     class Meta:
         db_table = 'mobileServer_cartitem'
-
+ 
 '''
 status:
 0 -> not ordered
@@ -45,29 +45,29 @@ status:
 '''
 class MobileserverOrder(models.Model):
     totalprice = models.FloatField(db_column='totalPrice', default=0.0)  # Field name made lowercase.
-    owner = models.ForeignKey('users.UsersCustomuser')
+    owner = models.ForeignKey('users.UsersCustomuser', on_delete=models.CASCADE)
     shop = models.ForeignKey('MobileserverShop', models.DO_NOTHING)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     status = models.PositiveSmallIntegerField(default=0)
-
+ 
     class Meta:
         db_table = 'mobileServer_order'
-
+ 
     def __str__(self):
         return self.id.__str__()+"\tOwner:"+self.owner.__str__()+\
                "\tFrom:"+self.shop.__str__()+"\tStatus:"+self.status.__str__()
-
-
+ 
+ 
 class MobileserverOrderProduct(models.Model):
     order = models.ForeignKey(MobileserverOrder)
     product = models.ForeignKey('MobileserverProduct', models.DO_NOTHING)
     quantity = models.PositiveSmallIntegerField(default=0)
     price = models.FloatField(default=0.0)
-
+ 
     class Meta:
         db_table = 'mobileServer_order_product'
-
+ 
 '''
 Category:
 0 ->
@@ -81,14 +81,14 @@ class MobileserverProduct(models.Model):
     category = models.PositiveSmallIntegerField()
     img = models.TextField()
     tags = TaggableManager()
-
+ 
     def __str__(self):
         return self.name+" by "+self.company
-
+ 
     class Meta:
         db_table = 'mobileServer_product'
-
-
+ 
+ 
 class address(models.Model):
     lat = models.FloatField()
     lon = models.FloatField()
@@ -99,23 +99,22 @@ class address(models.Model):
     mobile = models.IntegerField()
     owner = models.ForeignKey('users.UsersCustomuser')
     order = models.ForeignKey(MobileserverOrder, models.DO_NOTHING)
-
-
+ 
 class MobileserverShop(models.Model):
     name = models.CharField(max_length=30)
     rating = models.DecimalField(max_digits=10, decimal_places=5)  # max_digits and decimal_places have been guessed, as this database handles decimal fields as float
     lat = models.FloatField()
     lon = models.FloatField()
-
+ 
     def __str__(self):
         return self.name
-
+ 
     class Meta:
         db_table = 'mobileServer_shop'
-
+ 
     def __unicode__(self):
         return self.name
-
+ 
 #
 #
 # class MobileserverShopinventory(models.Model):
@@ -125,8 +124,8 @@ class MobileserverShop(models.Model):
 #
 #     class Meta:
 #         db_table = 'mobileServer_shopinventory'
-
-
+ 
+ 
 # class MobileserverShopinventoryProduct(models.Model):
 #     shopinventory = models.ForeignKey(MobileserverShopinventory, models.DO_NOTHING)
 #     shopproductinventory = models.ForeignKey('MobileserverShopproductinventory', models.DO_NOTHING)
@@ -134,47 +133,47 @@ class MobileserverShop(models.Model):
 #     class Meta:
 #         db_table = 'mobileServer_shopinventory_product'
 #         unique_together = (('shopinventory', 'shopproductinventory'),)
-
-
+ 
+ 
 class MobileserverShopproductinventory(models.Model):
     price = models.FloatField(default=0.0)
     stock = models.BooleanField(default=True)
-
-    product = models.ForeignKey(MobileserverProduct)
-    owner = models.ForeignKey('users.UsersCustomuser')
-    shop = models.ForeignKey(MobileserverShop)
-
+ 
+    product = models.ForeignKey(MobileserverProduct, on_delete=models.CASCADE)
+    owner = models.ForeignKey('users.UsersCustomuser', on_delete=models.CASCADE)
+    shop = models.ForeignKey(MobileserverShop, on_delete=models.CASCADE)
+ 
     class Meta:
         db_table = 'mobileServer_shopproductinventory'
-
+ 
     def __str__(self):
         return self.product.name+" "+self.shop.name+" "+str(self.price)+" "+str(self.stock)
-
-
-
-
-
-
-
+ 
+ 
+ 
+ 
+ 
+ 
+ 
 '''
-
+ 
 # Create your models here.
 class Shop(models.Model):
     name = models.CharField(max_length=30)
     rating = models.DecimalField(max_digits=5, decimal_places=1)
     lat = models.FloatField()
     lon = models.FloatField()
-
+ 
     def __str__(self):
         return self.name+" ("+self.lat.__str__()+","+self.lon.__str__()+")"
-
-
+ 
+ 
 '''
 # Category:
 # 1 ->
 # 2 ->
 # 3 ->
-
+ 
 '''
 class Product(models.Model):
     name = models.CharField(max_length=30)
@@ -182,14 +181,14 @@ class Product(models.Model):
     img = models.TextField(max_length=100)
     category = models.IntegerField()
     tags = TaggableManager()
-
+ 
     def __str__(self):
         return self.name+" by "+self.company
-
+ 
     def natural_key(self):
         return self.name, self.company, self.img, self.category
-
-
+ 
+ 
 '''
 # Class represents a cart, which can represent an order or a basket
 '''
@@ -197,25 +196,25 @@ class Cart(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     checked_out = models.BooleanField(default=False, verbose_name=_('checked out'))
-
+ 
     # User that owns the cart
     owner = models.ForeignKey(CustomUser)
-
+ 
     class Meta:
         verbose_name = _('cart')
         verbose_name_plural = _('carts')
         ordering = ('created_at',)
-
+ 
     def __unicode__(self):
         return self.creation_date
-
+ 
     def __str__(self):
         return "Created at:"+str(self.created_at)
-
+ 
     def natural_key(self):
         return self.cartitem_set.natural_key()
-
-
+ 
+ 
 """
 TODO: Check what does order represent....do I need it or cart is enoughauth_group_permissions
 """
@@ -224,16 +223,16 @@ class Order(models.Model):
     owner = models.ForeignKey(CustomUser)
     totalPrice = models.FloatField()
     shop = models.OneToOneField(Shop)
-
-
+ 
+ 
 class ShopProductInventory(models.Model):
     product = models.ForeignKey(Product)
     price = models.FloatField()
-
+ 
     def __str__(self):
         return str(self.product)+" for "+str(self.price)
-
-
+ 
+ 
 class CartItem(models.Model):
     # a link to the cart that this item lives in
     cart = models.ForeignKey(Cart, verbose_name=_('cart'))
@@ -242,21 +241,21 @@ class CartItem(models.Model):
     # # a link to the product this item represent
     # product = models.ForeignKey(Product)
     product = models.ForeignKey(ShopProductInventory)
-
+ 
     class Meta:
         verbose_name = _('cartItem')
-
+ 
     def total_price(self):
         return self.quantity * self.unit_price
     total_price = property(total_price)
-
+ 
     def natural_key(self):
         return self.quantity, self.product.natural_key()
-
+ 
     # product
     def get_product(self):
         return Product.objects.get(pk=self.product.id)
-
+ 
     def __str__(self):
         return str(self.cart)+" "+str(self.product)+" "+str(self.quantity)
     # def set_product(self, product):
@@ -264,8 +263,8 @@ class CartItem(models.Model):
     #     self.object_id = product.pk
     #
     # product = property(get_product, set_product)
-
-
+ 
+ 
 """
 A shop inventory is a list of products that are linked to a specific shop
 """
@@ -275,21 +274,20 @@ class ShopInventory(models.Model):
     stock = models.BooleanField(default=True)
     # Shop owner
     owner = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
-
+ 
     def __str__(self):
         return str(self.product)+" @ "+str(self.shop)+" "+str(self.stock)
-
+ 
     def natural_key(self):
         return self.stock
-
-
+ 
+ 
 class Basket(models.Model):
     name = models.CharField(max_length=30)
     cart = models.ForeignKey(Cart, null=True)
     owner = models.ForeignKey(CustomUser)
-
+ 
     def natural_key(self):
         return (self.name,) + self.cart.natural_key()
-
+ 
 '''
-
