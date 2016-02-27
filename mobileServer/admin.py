d2681e05873da2b34307f84433714624119efee4
aemam <aemam.douh@gmail.com>
2016-02-27 16:01:35 +0300

    Update

--- mobileServer/admin.py
+++ mobileServer/admin.py
@@ -26,6 +26,6 @@ admin.site.register(MobileserverBasket)
 admin.site.register(MobileserverShopproductinventory)
 admin.site.register(MobileserverOrder)
 admin.site.register(MobileserverOrderProduct)
-
+admin.site.register(Address)
 # admin.site.register(MobileserverShopinventoryProduct)
 #admin.site.unregister(Group)
