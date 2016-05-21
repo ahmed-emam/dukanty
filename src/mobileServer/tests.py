0a4fbe2dd78e7bb5058a94d0fda71ed41fa0f222
Mahmoud <mahmoud.alismail@gmail.com>
2016-05-21 11:26:45 +0000

    added test files and query_wrapper files

--- src/mobileServer/tests.py
+++ src/mobileServer/tests.py
@@ -3,7 +3,7 @@ from rest_framework.test import APIClient
 
 print "Testing Inventory API"
 client = APIClient()
-client.login(username='mahmoud.alismail@gmail.com', password='pass4mahmoud')
-response = client.post('/inventory/check_in/', {'shop_id':1, 'products_list': [{'id': 1, 'quantity':20}],}, format='json')
+# client.login(username='mahmoud.alismail@gmail.com', password='pass4mahmoud')
+response = client.post('/inventory/check_in/', {'shop_id':12, 'products_list': [{'id': 1, 'quantity':20}],}, format='json')
 print "Response Code", response.status_code
 print response.content
