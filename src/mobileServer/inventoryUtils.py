bb9f1f79ec19bd72ae19ef2167e9e0ba93c2bde2
Mahmoud <mahmoud.alismail@gmail.com>
2016-05-21 10:47:56 +0000

    checkin and checkout feature

--- src/mobileServer/inventoryUtils.py
+++ src/mobileServer/inventoryUtils.py
@@ -1,15 +1,14 @@
 from django.core.exceptions import *
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
+
 from rest_framework import status
 from rest_framework.renderers import JSONRenderer
 from rest_framework.decorators import api_view, permission_classes, authentication_classes
-
 from rest_framework.authentication import SessionAuthentication, TokenAuthentication
 from rest_framework.permissions import IsAuthenticated, AllowAny
 
 from mobileServer.serializer import *
-
 
 class JSONResponse(HttpResponse):
     """
@@ -19,59 +18,6 @@ class JSONResponse(HttpResponse):
         content = JSONRenderer().render(data)
         kwargs['content_type'] = 'application/json'
         super(JSONResponse, self).__init__(content, **kwargs)
-
-
-"""
-@api {GET} getshopinventory/:shop_id Get Shop Inventory
-@apiVersion 1.0.0
-@apiName GetShopInventory
-@apiGroup Shop
-@apiParam {Number} shop_id Shop unique ID.
-@apiSuccess {Object} product Product serialized data
-@apiSuccess {Number} price Product's price
-@apiSuccess {Boolean} Stock Product in Stock/out of stock
-@apiSuccess {Number} image_width Product's image width
-@apiSuccess {Number} image_height Product's image height
-
-@apiUse ShopNotFoundError
-"""
-@api_view(['GET'])
-@authentication_classes((TokenAuthentication,))
-@permission_classes((AllowAny,))
-def get_shopInventory(request):
-    print("******REQUEST*******")
-    print(request.body)
-    print(request.user)
-    print("*********************")
-
-    shop_id = request.GET.get('shop_id')
-
-    print(shop_id)
-    # Get the shop using shop ID
-    try:
-        shop = MobileserverShop.objects.get(pk=shop_id)
-
-    except ObjectDoesNotExist:
-        return JSONResponse({'error': shop_id+' does not exist'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-
-    # Get all inventory entries that are linked to that shop
-    inventory = MobileserverShopproductinventory.objects.filter(shop=shop)
-    # Response sent back to the user
-    response = []
-    # loop over all the inventory entries found
-    for inventoryEntry in inventory:
-        related_product = inventoryEntry.product
-        imageObject = Image.objects.get(product=related_product.id)
-        productSerialized = ProductSerializer(related_product)
-        response.extend([{
-            "product": productSerialized.data,
-            "price": inventoryEntry.price,
-            "stock": inventoryEntry.stock,
-            "image_width": imageObject.image.width,
-            "image_height": imageObject.image.height,
-        }])
-
-    return JSONResponse(response)
 
 
 '''
@@ -107,7 +53,6 @@ stock:  <In Stock/Out of Stock>
 """
 #TODO: Remove csrf_exempt
 #TODO: Accept list of products
-
 @csrf_exempt
 def create_inventory(request):
     print("******REQUEST*******")
@@ -151,3 +96,158 @@ def create_inventory(request):
         return JSONResponse(serializedData.data, status=status.HTTP_200_OK)
     except MultipleObjectsReturned:
         return JSONResponse({'error': 'Found multiple entries'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+"""
+@api {GET} /inventory/getshop/:shop_id Get Shop Inventory
+@apiVersion 1.0.0
+@apiName GetShopInventory
+@apiGroup Shop
+@apiParam {Number} shop_id Shop unique ID.
+@apiSuccess {Object} product Product serialized data
+@apiSuccess {Number} price Product's price
+@apiSuccess {Boolean} Stock Product in Stock/out of stock
+@apiSuccess {Number} image_width Product's image width
+@apiSuccess {Number} image_height Product's image height
+
+@apiUse ShopNotFoundError
+"""
+@api_view(['GET'])
+@authentication_classes((TokenAuthentication,))
+@permission_classes((AllowAny,))
+def get_shopInventory(request, shop_id):
+    print("******REQUEST*******")
+    print(request.body)
+    print(request.user)
+    print("*********************")
+
+    # shop_id = request.GET.get('shop_id')
+    shop_id = shop_id
+
+    print(shop_id)
+    # Get the shop using shop ID
+    try:
+        shop = MobileserverShop.objects.get(pk=shop_id)
+
+    except ObjectDoesNotExist:
+        return JSONResponse({'Error': 'shop with id '+shop_id+' does not exist'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    # Get all inventory entries that are linked to that shop
+    inventory = MobileserverShopproductinventory.objects.filter(shop=shop)
+    # Response sent back to the user
+    response = []
+    # loop over all the inventory entries found
+    for inventoryEntry in inventory:
+        related_product = inventoryEntry.product
+        imageObject = Image.objects.get(product=related_product.id)
+        productSerialized = ProductSerializer(related_product)
+        # why are you extending here?
+        response.extend([{
+            "product": productSerialized.data,
+            "price": inventoryEntry.price,
+            "stock": inventoryEntry.stock,
+            "image_width": imageObject.image.width,
+            "image_height": imageObject.image.height,
+        }])
+
+    return JSONResponse(response)
+
+
+"""
+@api {POST} /inventory/check_in Check in products 
+@apiVersion 1.0.0
+@apiName check_in
+@apiGroup Shop
+The ones below are incorrect, will come back to them in a moment
+@apiSuccess {Object} product Product serialized data
+@apiSuccess {Number} price Product's price
+@apiSuccess {Boolean} Stock Product in Stock/out of stock
+@apiSuccess {Number} image_width Product's image width
+@apiSuccess {Number} image_height Product's image height
+
+@apiUse ShopNotFoundError
+"""
+
+@api_view(['POST'])
+@authentication_classes((TokenAuthentication,))
+@permission_classes((AllowAny,))
+def check_in(request):
+    user = request.user
+    # check user is authenticated
+    # check user is owner/superuser
+
+    # tying/catching does not always work because you will get none
+    try:
+        shop_id = request.data['shop_id']
+        products_list = request.data['products_list']
+    except KeyError:
+        return JSONResponse({'error': 'Request Missing Parameters'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    
+    # got the parameters, lets check them in
+    # assumes that shop and all products already exist
+    for product in products_list:
+        # product = {'id': 50, 'quantity': 10}
+        # line below might not work, needs to be tested
+        try:
+            shop_db = MobileserverShop.objects.get(pk=shop_id)
+            product_db = MobileserverProduct.objects.get(pk=product['id'])
+            inventory_product = MobileserverShopproductinventory.objects.get(shop=shop_db, product=product_db)
+        except ObjectDoesNotExist:
+            return JSONResponse({'error': 'Either shop or product do not exist'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        
+        print "Current stock", inventory_product.stock
+        inventory_product.stock += product['quantity']
+        # timestamp will be updated on save
+        inventory_product.save()
+
+    # need to change the type of the response
+    return JSONResponse({'success'})
+
+
+"""
+@api {POST} /inventory/check_out Check out products 
+@apiVersion 1.0.0
+@apiName check_out
+@apiGroup Shop
+The ones below are incorrect, will come back to them in a moment
+@apiSuccess {Object} product Product serialized data
+@apiSuccess {Number} price Product's price
+@apiSuccess {Boolean} Stock Product in Stock/out of stock
+@apiSuccess {Number} image_width Product's image width
+@apiSuccess {Number} image_height Product's image height
+
+@apiUse ShopNotFoundError
+"""
+@api_view(['POST'])
+@authentication_classes((TokenAuthentication,))
+@permission_classes((AllowAny,))
+def check_out(request):
+    user = request.user
+    # check user is authenticated
+    # check user is owner/superuser
+
+    # tying/catching does not always work because you will get none
+    try:
+        shop_id = request.data['shop_id']
+        products_list = request.data['products_list']
+    except KeyError:
+        return JSONResponse({'error': 'Request Missing Parameters'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    
+    # got the parameters, lets check them in
+    # assumes that shop and all products already exist
+    for product in products_list:
+        # product = {'id': 50, 'quantity': 10}
+        # line below might not work, needs to be tested
+        try:
+            shop_db = MobileserverShop.objects.get(pk=shop_id)
+            product_db = MobileserverProduct.objects.get(pk=product['id'])
+            inventory_product = MobileserverShopproductinventory.objects.get(shop=shop_db, product=product_db)
+        except ObjectDoesNotExist:
+            return JSONResponse({'error': 'Either shop or product do not exist'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        
+        inventory_product.stock -= product['quantity']
+        # timestamp will be updated on save
+        inventory_product.save()
+
+    # need to change the type of the response
+    return JSONResponse({'success'})
